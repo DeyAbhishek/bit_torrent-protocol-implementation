788fb3dab1d7b490dd5e3a7923a310faf2e24baf
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-02-26 23:50:44 -0500

    Update and rename src to src/ByteIntConversion.java

---
+++ src/ByteIntConversion.java
@@ -0,0 +1,31 @@
+/*
+ * This class is used to convert a byte array to int and vice versa
+ */
+public class ByteIntConversion {
+	//Converts byte array to int
+	public static int byteArrayToInt(byte[] b) 
+	{
+	    int value = 0;
+	    for (int i = 0; i < 4; i++) {
+	        int shift = (4 - 1 - i) * 8;
+	        value += (b[i] & 0x000000FF) << shift;
+	    }
+	    return value;
+	}
+	//Converts int to byte array
+	public static byte[] intToByteArray(int a)
+	{
+	    byte[] ret = new byte[4];
+	    ret[3] = (byte) (a & 0xFF);   
+	    ret[2] = (byte) ((a >> 8) & 0xFF);   
+	    ret[1] = (byte) ((a >> 16) & 0xFF);   
+	    ret[0] = (byte) ((a >> 24) & 0xFF);
+	    return ret;
+	}
+	//for testing
+	/*public static void main(String[] args){
+		int a = 23;
+		System.out.println(byteArrayToInt(intToByteArray(a)));
+	}*/
+
+}
